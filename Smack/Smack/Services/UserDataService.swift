db1f340f1ce29754cf73220ff403157e466db540
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-29 02:35:48 +0300

    completed created a user

---
+++ Smack/Smack/Services/UserDataService.swift
@@ -0,0 +1,32 @@
+//
+//  UserDataService.swift
+//  Smack
+//
+//  Created by Admin on 8/29/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class UserDataService {
+    static let instance = UserDataService()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData(id: String, avatarColor: String, avatarName: String, email: String, name: String) {
+        self.id = id
+        self.avatarColor = avatarColor
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+}
