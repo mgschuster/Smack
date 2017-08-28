67301829827841eb13522e03a60e503c2a624bbc
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-28 17:11:27 +0300

    finished LoginVC UI

---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Smack
+//
+//  Created by Admin on 8/28/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
