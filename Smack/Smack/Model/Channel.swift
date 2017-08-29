b27d5f0474818f1e0734b26d7fdda9bd570757d4
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-29 18:37:11 +0300

    finished getting channels

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Admin on 8/29/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
---
+++ Smack/Smack/Services/MessageService.swift
@@ -0,0 +1,39 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Admin on 8/29/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                if let json = JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    completion(true)
+                }
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
